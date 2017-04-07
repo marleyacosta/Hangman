ac73111bc5ea6bf36f1e3f1c3db4eaf4e118412e
Maurely Regina Acosta Olivas <macos101@fiu.edu>
2017-04-06 22:41:48 -0400

    Using movie database now and changing file structure
    Using movie database now and changing file structure

--- hangman_home.py
+++ hangman_home.py
@@ -1,20 +1,29 @@
 
 from tkinter import *
 import tkinter.messagebox
+import sqlite3
+
+
+
+'''
+
+# Calculate the number of turns depending on how distinct letters in the movie title
+num_distinct_letters = len(''.join(set(movie_title)))
+turns = num_distinct_letters + round(num_distinct_letters * .25)
+print(movie_title)
+
+'''
 
 ## letter guess section ##
-def letterguess(guessfield, hiddenword, game, gamelabel1, man):
+def letterguess(guessfield, hiddenmovie, game, gamelabel1, man):
 	global correctcounter
 	global incorrectcounter
-	global wordarray
+	global moviearray
 	global guessedletters
-	global word
-
-
-
+	global movie
 
 	i = 0
-	letterinword = False
+	letterinmovie = False
 	valid = True
 	letter = guessfield.get()
 	letter = letter.lower()
@@ -26,20 +35,20 @@ def letterguess(guessfield, hiddenword, game, gamelabel1, man):
 		valid = False
 
 	## check to see if letter is good ##
-	while (i < len(word) and valid):
-		if word[i] == letter[0]:
+	while (i < len(movie) and valid):
+		if movie[i] == letter[0]:
 
-			letterinword = True
-			wordarray.pop(2*i)
-			wordarray.insert(2*i, letter[0])
+			letterinmovie = True
+			moviearray.pop(2*i)
+			moviearray.insert(2*i, letter[0])
 
 			if guessedletters.count(letter[0]) == 0:
-				correctcounter = correctcounter + word.count(letter[0])
+				correctcounter = correctcounter + movie.count(letter[0])
 				guessedletters.append(letter[0])
 		i = i + 1
 
 	## incorrect guess ##
-	if (not letterinword and valid):
+	if (not letterinmovie and valid):
 
 		if guessedletters.count(letter[0]) == 0:
 			guessedletters.append(letter[0])
@@ -47,66 +56,96 @@ def letterguess(guessfield, hiddenword, game, gamelabel1, man):
 
 	## update label ##
 	remainingguesses = 6 - incorrectcounter
-	wordprint = ''.join(wordarray) + "\n"
-	wordguessedprint = ''.join(guessedletters)
-	wordprint = wordprint + "\nGuessed Letters: \n" + wordguessedprint + "\n"
-	wordprint = wordprint + "\nIncorrect Guesses Remaining: " + str(remainingguesses) + "\n"
-	hiddenword.set(wordprint)
+	movieguessedprint = ''.join(guessedletters)
+	movieprint = ''.join(moviearray) + "\n"
+
+	movieprint = movieprint + "\nGuessed Letters: \n" + movieguessedprint + "\n"
+	movieprint = movieprint + "\nIncorrect Guesses Remaining: " + str(remainingguesses) + "\n"
+	hiddenmovie.set(movieprint)
 
 	## update image ##
 	showhangman(gamelabel1, remainingguesses)
 
 	## win condition ##
-	if correctcounter == len(word):
-		win(game, word)
+	if correctcounter == len(movie):
+		win(game, movie)
 
 	## lose condition ##
 	if incorrectcounter >= 6:
-		lose(game, word)
+		lose(game, movie)
 
-## word guess section ##
-def wordguess(guessfield, hiddenword, game, man):
+## movie guess section ##
+def movieguess(guessfield, hiddenmovie, game, man):
 
-	global word
-	wordguess = guessfield.get()
+	global movie
+
+	movieguess = guessfield.get()
 	i = 0
 	match = True
 
-	if (len(wordguess) == 0):
+	if (len(movieguess) == 0):
 		match = False
 
-	while i < len(wordguess):
-		if word[i] != wordguess[i]:
+	while i < len(movieguess):
+		if movie[i] != movieguess[i]:
 			match = False
 		i = i + 1
 
-	if len(word) != len(wordguess):
+	if len(movie) != len(movieguess):
 		match = False
 
 	if match:
-		win(game, word)
+		win(game, movie)
 	else:
-		lose(game, word)
+		lose(game, movie)
+
+def hint():
+	hint = "The movie was released in " + title_year + " and directed by " + director_name + ".\n" + "Starring " + actor_1_name + ", " + actor_2_name + ", and " + actor_3_name + "."
+	tkinter.messagebox.showinfo("Hint", hint)
 
 
 ## play the actual game ##
 def startgame():
 
-	global word
-	word = "maurely"
+	global movie
+	global director_name
+	global actor_1_name
+	global actor_2_name
+	global actor_3_name
+	global title_year
 
+	# Get the movie
+	cursor = movie_metadata_connection.cursor()
 
+	# This generated a random row from the movie_metadata database
+	cursor.execute(
+		"SELECT * " +
+		 "FROM movie_metadata " +
+		 "ORDER BY RANDOM() " +
+		 "LIMIT 1")
+
+	results = cursor.fetchall()
+
+	row = results[0]
+
+	# Get the data from the row.
+	director_name = row[1]
+	actor_1_name = row[3]
+	actor_2_name = row[2]
+	actor_3_name = row[5]
+	title_year = row[6]
+	movie = row[4]
 
 	## lots of variables for actual game ##
-	global wordarray
-	wordarray = []
+	global  moviearray
+	moviearray = []
 	global guessedletters
 	guessedletters = []
 
 	i = 0
-	while i < len(word):
-		wordarray.append('_')
-		wordarray.append(' ')
+	while i < len(movie):
+		moviearray.append('_')
+		moviearray.append(' ')
 		i = i + 1
 
 	global correctcounter
@@ -122,44 +161,47 @@ def startgame():
 	game.geometry("500x450")
 
 	man = PhotoImage(file="hangman_pics/gallows.gif")
-	hiddenword = StringVar()
+	hiddenmovie = StringVar()
 	gamelabel1 = Label(game, image=man)
 	gamelabel1.image = man
 	gamelabel1.pack()
 
-	gamelabel2 = Label(game, textvariable=hiddenword)
+	gamelabel2 = Label(game, textvariable=hiddenmovie)
 	gamelabel2.pack()
 
 	guessfield = Entry(game)
 	guessfield.pack()
 
 	remainingguesses = 6 - incorrectcounter
-	wordprint = ''.join(wordarray)
-	wordguessedprint = ''.join(guessedletters)
-	wordprint = wordprint + "\nGuessed Letters: " + wordguessedprint + "\n"
-	wordprint = wordprint + "\nIncorrect Guesses Remaining: " + str(remainingguesses) + "\n"
-	hiddenword.set(wordprint)
+	movieprint = ''.join(moviearray)
+	movieguessedprint = ''.join(guessedletters)
+	movieprint = movieprint + "\nGuessed Letters: " + movieguessedprint + "\n"
+	movieprint = movieprint + "\nIncorrect Guesses Remaining: " + str(remainingguesses) + "\n"
+	hiddenmovie.set(movieprint)
 
-	bguessletter = Button(game, text="Guess Letter", width=15, command=lambda:
-		letterguess(guessfield, hiddenword, game, gamelabel1, man))
+	bguessletter = Button(game, text="Guess Letter", width=15, command=lambda:letterguess(guessfield, hiddenmovie, game, gamelabel1, man))
 	bguessletter.pack()
 
-	bguessword = Button(game, text="Guess Word [ONE CHANCE]", width=25, command=lambda:wordguess(guessfield, hiddenword, game, man))
-	bguessword.pack()
+	bguessmovie = Button(game, text="Guess Word [ONE CHANCE]", width=25, command=lambda:movieguess(guessfield, hiddenmovie, game, man))
+	bguessmovie.pack()
+
+	bhint = Button(game, text="Hint", width=15, command=hint)
+	bhint.pack()
 
 	game.mainloop()
 
 ## quit the game ##
 def quitnow():
 	tkinter.messagebox.showinfo("Movies Hangman", "Thanks for playing! See you soon!")
+	movie_metadata_connection.close()
 	exit()
 
-def win(game, word):
-	tkinter.messagebox.showinfo("Winnerx2-Chicken-Dinner", "You WIN! The movie was " + word + "!")
+def win(game, movie):
+	tkinter.messagebox.showinfo("Winnerx2-Chicken-Dinner", "You WIN! The movie was " + movie + "!")
 	game.withdraw()
 
-def lose(game, word):
-	tkinter.messagebox.showinfo("Loser-Shmooser", "You LOSE! The movie was " + word + "!")
+def lose(game, movie):
+	tkinter.messagebox.showinfo("Loser-Shmooser", "You LOSE! The movie was " + movie + "!")
 	game.withdraw()
 
 def showhangman(gamelabel1, remainingguesses):
@@ -192,8 +234,12 @@ def showhangman(gamelabel1, remainingguesses):
 		gamelabel1.configure(image = img)
 		gamelabel1.image = img
 
+# Connect to the movie database
+global movie_metadata_connection
+movie_metadata_connection = sqlite3.connect('database/movie_metadata.sqlite')
+
 root = Tk()
-root.wm_title("Hangman in Python, by jl")
+root.wm_title("Movies Hangman")
 root.minsize(380,380)
 root.geometry("300x100")
 
